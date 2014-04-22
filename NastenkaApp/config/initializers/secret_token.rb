03ae202225f613eda7bb67753306bae2f4e28781
mato <mato@devRails.(none)>
2014-04-22 09:49:10 +0200

    Initial Nastenka App

---
+++ NastenkaApp/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NastenkaApp::Application.config.secret_key_base = '567746db86b5cd916b8480080b231214231eaab812c8be9b40cb282bad2df072fe474e6cb04521ebcbbe3b1897c55515c0ff309bd0df49c4edc104173ce19b3e'
